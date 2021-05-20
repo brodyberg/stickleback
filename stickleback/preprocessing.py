ee067aa80346135db41ab6230e76da927dde2af2
Max Czapanskiy <maxczap@stanford.edu>
2021-05-19 17:53:02 -0700

    Moved extract_all to preprocessing.py and added test

--- stickleback/preprocessing.py
+++ stickleback/preprocessing.py
@@ -1,13 +1,20 @@
 import pandas as pd
 import numpy as np
+from pandas.core.frame import DataFrame
 from sktime.utils.data_processing import from_3d_numpy_to_nested
-from typing import Dict
+from typing import Dict, Tuple
 
 def extract_all(sensors: Dict[str, pd.DataFrame], nth: int, win_size: int) -> pd.DataFrame:
     win_size_2 = int(win_size / 2)
+    idx = {d: sensors[d].iloc[win_size_2:-win_size_2:nth].index for d in sensors}
+    return extract_nested(sensors, idx, win_size)
 
-    def _extract(_deployid: str, _sensors: pd.DataFrame):
-        idx = np.arange(win_size_2, len(_sensors) - win_size_2, nth)
+def extract_nested(sensors: Dict[str, pd.DataFrame], idx: Dict[str, pd.DatetimeIndex], win_size: int) -> pd.DataFrame:
+    win_size_2 = int(win_size / 2)
+
+    def _extract(_deployid: str, _idx: pd.DatetimeIndex):
+        _sensors = sensors[_deployid]
+        idx = _sensors.index.get_indexer(_idx)
         data_3d = np.empty([len(idx), len(_sensors.columns), win_size], float)
         data_arr = _sensors.to_numpy().transpose()
         start_idx = idx - win_size_2
@@ -18,5 +25,5 @@ def extract_all(sensors: Dict[str, pd.DataFrame], nth: int, win_size: int) -> pd
         nested.index = pd.MultiIndex.from_product([[_deployid], _sensors.index[idx]])
         return nested
 
-    return pd.concat([_extract(d, s) for d, s in sensors.items()])
+    return pd.concat([_extract(d, i) for d, i in idx.items()])
     
--- tests/teststickleback.py
+++ tests/teststickleback.py
@@ -1,26 +1,24 @@
 import pandas as pd
 import numpy as np
 import unittest
-from stickleback.preprocessing import extract_all
+from stickleback.preprocessing import extract_all, extract_nested
+
+idx1 = pd.date_range(start="2020-01-01 00:00", freq="S", periods=10, tz="Etc/GMT+7")
+idx2 = pd.date_range(start="2020-02-01 00:00", freq="S", periods=10, tz="Etc/GMT+3")
+sensors = {
+        "d1": pd.DataFrame({
+            "a": np.arange(10),
+            "b": np.arange(10) ** 2
+        }, index = idx1),
+        "d2": pd.DataFrame({
+            "a": -np.arange(10),
+            "b": -(np.arange(10) ** 2)
+        }, index = idx2)
+    }
 
 class ExtractTestCase(unittest.TestCase):
     
     def test_extract_all(self):
-        idx1 = pd.date_range(start="2020-01-01 00:00", 
-                             freq="S", periods=10, tz="Etc/GMT+7")
-        idx2 = pd.date_range(start="2020-02-01 00:00", 
-                             freq="S", periods=10, tz="Etc/GMT+3")
-        sensors = {
-                "d1": pd.DataFrame({
-                    "a": np.arange(10),
-                    "b": np.arange(10) ** 2
-                }, index = idx1),
-                "d2": pd.DataFrame({
-                    "a": -np.arange(10),
-                    "b": -(np.arange(10) ** 2)
-                }, index = idx2)
-            }
-
         result = extract_all(sensors, nth=1, win_size=3)
 
         # Size and contents
@@ -33,3 +31,11 @@ class ExtractTestCase(unittest.TestCase):
         self.assertEqual(result.index.nlevels, 2)
         self.assertEqual(set(result.index.get_level_values(0)), set(sensors.keys()))
         self.assertTrue(np.array_equal(result.loc["d1"].index, idx1[1:-1]))
+
+    def test_extract_nested(self):
+        idx = {"d1": idx1[[2, 4]], "d2": idx2[[3, 5]]}
+        result = extract_nested(sensors, idx, win_size=3)
+
+        self.assertEqual(len(result), 4)
+        result_idx = pd.MultiIndex.from_tuples([(d, i) for d in idx for i in idx[d]])
+        self.assertTrue(np.array_equal(result.index, result_idx))
